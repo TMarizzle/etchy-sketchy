187890aea460d304d379dafabbbf5dafc7a08758
Tyler <6041421+TMarizzle@users.noreply.github.com>
2025-08-01 14:57:20 -0500

    Changed default number of squares to 8

--- etchy.js
+++ etchy.js
@@ -1,21 +1,9 @@
 const squares = document.querySelector(".squaresContainer");
 //set number of squares to draw, but needs to be Row x Col (4x4)
-const numSquares = 4;
+const numSquares = 8;
 let i = 1;
 
 function drawSquares(num){
-    // while (i <= num){
-    //     //draw squares and increase counter
-    //     const newSquare = document.createElement("div");
-    //     newSquare.classList = `squares row${i}`;
-    //     //add mouseover for each div to change background color
-    //     newSquare.addEventListener('mouseover', () => {
-    //         newSquare.style.backgroundColor = "green";
-    //     });
-    //     squares.appendChild(newSquare);
-    //     console.log("Drawing square!");
-    //     i++;
-    // }
     for (let i = 1; i <= num; i++){
         let newRow = document.createElement("div");
         newRow.classList = `row${i}`;
