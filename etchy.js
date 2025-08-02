1d2d94295875f586317a9b8115a57fedaefc7130
Tyler <6041421+TMarizzle@users.noreply.github.com>
2025-08-01 19:23:07 -0500

    Adjusted buttons and css; resizing square divs isn't filling out allowed area yet

--- etchy.js
+++ etchy.js
@@ -3,29 +3,16 @@ const resetBtn = document.getElementById("reset").addEventListener('click', () =
     location.reload();
 });
 const sixteenBtn = document.getElementById("sixteen").addEventListener('click', () => {
-    if (squares.children.length === 0){
-        drawSquares(16);
-    } else{
         removeSquares();
         drawSquares(16);
-    }
 });
 const eightBtn = document.getElementById("eight").addEventListener('click', () => {
-    //check if grid already exists; if it does, erase and then redraw a new one
-    if (squares.children.length === 0){
-        drawSquares(8);
-    } else{
         removeSquares();
         drawSquares(8);
-    }
 });
 const fortyBtn = document.getElementById("forty").addEventListener('click', () => {
-    if (squares.children.length === 0){
-        drawSquares(48);
-    } else{
         removeSquares();
         drawSquares(48);
-    }
 });
 
 function drawSquares(num){
